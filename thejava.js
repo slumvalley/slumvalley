3526043442b0821698bcbf165a8369ec7868e2eb
slumvalley <bloody.boots@hotmail.com>
2016-05-04 19:12:39 -0700

    Update thejava.js

--- thejava.js
+++ thejava.js
@@ -8,7 +8,7 @@ function init() {
   'use strict';
 
 
- document.getElementById('theForm').onsubmit = function() {addEmployee();};
+ document.getElementById('theForm').onsubmit = addEmployee();
   return false;
 }
 
