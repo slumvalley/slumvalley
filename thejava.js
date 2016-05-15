5e6ced22a179c82ceafeedb0e01f07359a1a7b6a
slumvalley <bloody.boots@hotmail.com>
2016-05-15 02:18:34 -0700

    Update thejava.js

--- thejava.js
+++ thejava.js
@@ -10,7 +10,6 @@ function init() {
 
  document.getElementById('theForm').onsubmit = addEmployee;
  
- return false;
   
 }
 
@@ -26,14 +25,14 @@ function addEmployee() {
   
   var idnum = getID();
   
-  emp.employeeID = idnum;
+
  
   customers[num] = emp;
   console.log(customers[num].first);
   console.log(customers[num].last);
   console.log(customers[num].email);
-  console.log(customers[num].employeeID);
-  var d = Date();
+
+
   num+=1;
   
  
@@ -41,22 +40,4 @@ function addEmployee() {
   
   return false;
 }
-
-function getID() {
-  
-  var temp = Math.floor(10000000 + Math.random() * 90000000);
-  for(var i=0; i < num;i++){
-    if(temp!=customers[i].id){
-      break;
-    }
-    else {
-      temp = getID();
-    }
-  }
-  
-  return temp;
-  
-  
-}
-
 
