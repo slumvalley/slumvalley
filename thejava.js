b85825274ac2cfc9bf460297a635fae62182991f
slumvalley <bloody.boots@hotmail.com>
2016-05-15 02:39:41 -0700

    Update thejava.js

--- thejava.js
+++ thejava.js
@@ -8,8 +8,7 @@ function init() {
   'use strict';
 
 
-document.getElementById('theForm').onsubmit = addEmployee;
-
+ document.getElementById('theForm').onsubmit = function() {addEmployee();};
   
 }
 
@@ -25,20 +24,39 @@ function addEmployee() {
   
   var idnum = getID();
   
-
+  emp.employeeID = idnum;
  
   customers[num] = emp;
   console.log(customers[num].first);
   console.log(customers[num].last);
   console.log(customers[num].email);
-
-
+  console.log(customers[num].employeeID);
+  var d = Date();
   num+=1;
   
-  alert("hello");
- 
+ document.getElementById("hi").innerHTML=("<h4>You Have Successfully Registered</h4>  <br> Name: " + customers[num-1].last + ", " + customers[num-1].first + "<br> Email: " + customers[num-1].email + "<br> Confirmation Number: " + customers[num-1].employeeID + "<br> Hire Date: " + d + "<br> Number of Employees: " + num);
+  
   console.log(customers);
   
   return false;
 }
+
+function getID() {
+  
+  var temp = Math.floor(10000000 + Math.random() * 90000000);
+  for(var i=0; i < num;i++){
+    if(temp!=customers[i].id){
+      break;
+    }
+    else {
+      temp = getID();
+    }
+  }
+  
+  return temp;
+  
+  
+}
+
+
 
