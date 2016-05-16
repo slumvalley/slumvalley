895b227a35056d6555450f01118d8b188e31243e
slumvalley <bloody.boots@hotmail.com>
2016-05-16 01:03:00 -0700

    Update thejava.js

--- thejava.js
+++ thejava.js
@@ -114,6 +114,7 @@ function addToCart() {
       }
       else {
         document.getElementById("demo").innerHTML = "There are no items selected.";
+        document.getElementById("cart").innerHTML= "";
         
       }
       
