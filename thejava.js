5682ceb0d7c9f3b09c6444aa1c735f21c904f432
slumvalley <bloody.boots@hotmail.com>
2016-05-18 21:45:08 -0700

    Update thejava.js

--- thejava.js
+++ thejava.js
@@ -219,23 +219,23 @@ document.getElementById("time").innerHTML = "";
 }
 
 // show the given page, hide the rest
-            function show(elementID) {
-                // try to find the requested page and alert if it's not found
-                var ele = document.getElementById(elementID);
-                if (!ele) {
-                    alert("no such element");
-                    return;
-                }
+ function show(elementID) {
+ // try to find the requested page and alert if it's not found
+ var ele = document.getElementById(elementID);
+ if (!ele) {
+ alert("no such element");
+ return;
+}
 
-                // get all pages, loop through them and hide them
-                var pages = document.getElementsByClassName('page');
-                for(var i = 0; i < pages.length; i++) {
-                    pages[i].style.display = 'none';
-                }
+// get all pages, loop through them and hide them
+var pages = document.getElementsByClassName('page');
+for(var i = 0; i < pages.length; i++) {
+pages[i].style.display = 'none';
+ }
 
-                // then show the requested page
-                ele.style.display = 'block';
-            }
+// then show the requested page
+ ele.style.display = 'block';
+}
 
 
 
